7180157079ecb852c0297e602d1b25b6f77799a2
Dmytro Boichenko <dmytro.boichenko.dev@gmail.com>
2020-05-03 19:09:59 +0200

    All DB methods were rewritten with RxJava

--- src/main/java/io/vertx/guides/wiki/db/WikiDatabaseServiceImpl.java
+++ src/main/java/io/vertx/guides/wiki/db/WikiDatabaseServiceImpl.java
@@ -2,11 +2,11 @@ package io.vertx.guides.wiki.db;
 
 import io.reactivex.Single;
 import io.vertx.core.AsyncResult;
-import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.sql.ResultSet;
+import io.vertx.reactivex.CompletableHelper;
 import io.vertx.reactivex.MaybeHelper;
 import io.vertx.reactivex.SingleHelper;
 import io.vertx.reactivex.ext.jdbc.JDBCClient;
@@ -42,20 +42,14 @@ public class WikiDatabaseServiceImpl implements WikiDatabaseService {
 
     @Override
     public WikiDatabaseService fetchAllPages(Handler<AsyncResult<JsonArray>> resultHandler) {
-        dbClient.query(sqlQueries.get(SqlQuery.ALL_PAGES), res -> {
-            if (res.succeeded()) {
-                JsonArray pages = new JsonArray(res.result()
-                    .getResults()
-                    .stream()
-                    .map(json -> json.getString(0))
-                    .sorted()
-                    .collect(Collectors.toList()));
-                resultHandler.handle(Future.succeededFuture(pages));
-            } else {
-                LOGGER.error("Database query error", res.cause());
-                resultHandler.handle(Future.failedFuture(res.cause()));
-            }
-        });
+        dbClient.rxQuery(sqlQueries.get(SqlQuery.ALL_PAGES))
+            .map(rs -> new JsonArray(rs.getResults()
+                .stream()
+                .map(json -> json.getString(0))
+                .sorted()
+                .collect(Collectors.toList())))
+            .doOnError(e -> LOGGER.error("Database query error", e))
+            .subscribe(SingleHelper.toObserver(resultHandler));
         return this;
     }
 
@@ -71,24 +65,21 @@ public class WikiDatabaseServiceImpl implements WikiDatabaseService {
     public WikiDatabaseService fetchPage(String name, Handler<AsyncResult<JsonObject>> resultHandler) {
         JsonArray params = new JsonArray().add(name);
 
-        dbClient.queryWithParams(sqlQueries.get(SqlQuery.GET_PAGE), params, fetch -> {
-            if (fetch.succeeded()) {
+        dbClient.rxQueryWithParams(sqlQueries.get(SqlQuery.GET_PAGE), params)
+            .map(rs -> {
                 JsonObject response = new JsonObject();
-                ResultSet resultSet = fetch.result();
-                if (resultSet.getNumRows() == 0) {
+                if (rs.getNumRows() == 0) {
                     response.put("found", false);
                 } else {
                     response.put("found", true);
-                    JsonArray row = resultSet.getResults().get(0);
+                    JsonArray row = rs.getResults().get(0);
                     response.put("id", row.getInteger(0));
                     response.put("rawContent", row.getString(1));
                 }
-                resultHandler.handle(Future.succeededFuture(response));
-            } else {
-                LOGGER.error("Database query error", fetch.cause());
-                resultHandler.handle(Future.failedFuture(fetch.cause()));
-            }
-        });
+                return response;
+            })
+            .doOnError(e -> LOGGER.error("Database query error", e))
+            .subscribe(SingleHelper.toObserver(resultHandler));
         return this;
     }
 
@@ -109,6 +100,7 @@ public class WikiDatabaseServiceImpl implements WikiDatabaseService {
                 }
                 return response;
             })
+            .doOnError(e -> LOGGER.error("Database query error", e))
             .subscribe(MaybeHelper.toObserver(resultHandler));
         return this;
     }
@@ -119,14 +111,10 @@ public class WikiDatabaseServiceImpl implements WikiDatabaseService {
             .add(title)
             .add(markdown);
 
-        dbClient.updateWithParams(sqlQueries.get(SqlQuery.CREATE_PAGE), data, res -> {
-            if (res.succeeded()) {
-                resultHandler.handle(Future.succeededFuture());
-            } else {
-                LOGGER.error("Database create page error", res.cause());
-                resultHandler.handle(Future.failedFuture(res.cause()));
-            }
-        });
+        dbClient.rxUpdateWithParams(sqlQueries.get(SqlQuery.CREATE_PAGE), data)
+            .ignoreElement()
+            .doOnError(e -> LOGGER.error("Database update error", e))
+            .subscribe(CompletableHelper.toObserver(resultHandler));
         return this;
     }
 
@@ -136,14 +124,10 @@ public class WikiDatabaseServiceImpl implements WikiDatabaseService {
             .add(markdown)
             .add(id);
 
-        dbClient.updateWithParams(sqlQueries.get(SqlQuery.SAVE_PAGE), data, res -> {
-            if (res.succeeded()) {
-                resultHandler.handle(Future.succeededFuture());
-            } else {
-                LOGGER.error("Database saving page error", res.cause());
-                resultHandler.handle(Future.failedFuture(res.cause()));
-            }
-        });
+        dbClient.rxUpdateWithParams(sqlQueries.get(SqlQuery.SAVE_PAGE), data)
+            .ignoreElement()
+            .doOnError(e -> LOGGER.error("Database update error", e))
+            .subscribe(CompletableHelper.toObserver(resultHandler));
         return this;
     }
 
@@ -151,14 +135,10 @@ public class WikiDatabaseServiceImpl implements WikiDatabaseService {
     public WikiDatabaseService deletePage(int id, Handler<AsyncResult<Void>> resultHandler) {
         JsonArray data = new JsonArray().add(id);
 
-        dbClient.updateWithParams(sqlQueries.get(SqlQuery.DELETE_PAGE), data, res -> {
-            if (res.succeeded()) {
-                resultHandler.handle(Future.succeededFuture());
-            } else {
-                LOGGER.error("Database deletion page error", res.cause());
-                resultHandler.handle(Future.failedFuture(res.cause()));
-            }
-        });
+        dbClient.rxUpdateWithParams(sqlQueries.get(SqlQuery.DELETE_PAGE), data)
+            .ignoreElement()
+            .doOnError(e -> LOGGER.error("Database update error", e))
+            .subscribe(CompletableHelper.toObserver(resultHandler));
         return this;
     }
 }
